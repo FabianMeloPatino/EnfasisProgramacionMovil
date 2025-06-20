3bcf64293016b5124b8331146d51789cc92fd5ab
Fabian Melo <famepa86@gmail.com>
2025-06-20 16:56:20 -0500

    Sincronización completa del proyecto calma-campus

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calma-campus',
+  webDir: 'www'
+};
+
+export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonButton,
-  IonCard,
-  IonCardContent
-} from '@ionic/angular/standalone';
-import { RouterModule } from '@angular/router';
-import { NgForOf } from '@angular/common'; // 👈 Importa NgForOf para *ngFor
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
-  imports: [
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonButton,
-    IonCard,
-    IonCardContent,
-    RouterModule,
-    NgForOf // 👈 Agrega aquí también
-  ]
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor() {}
